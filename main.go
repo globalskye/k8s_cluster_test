f37aa72b8eb2acfa9c98230e0a73c80893d041b6
globalskye <43073544+globalskye@users.noreply.github.com>
2022-11-22 20:23:28 +0300

    test_21

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello"))
+		w.Write([]byte("hello 2"))
 	})
 	server := http.Server{Addr: ":1111", Handler: mux}
 	if err := server.ListenAndServe(); err != nil {
